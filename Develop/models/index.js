cbb55803c3791abc754d37609187fef392551161
Gabriela <gabriela.reyna89@icloud.com>
2023-04-28 16:58:42 -0400

    importing models, and products on index.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -3,14 +3,19 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const sequelize = require('../config/connection');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {foreignKey: 'category_id' });
 
 // Categories have many Products
+Category.hasMany(Product, {foreignKey:'product_id'});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, { through: 'ProductTag', foreignKey: 'product_id' });
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: 'ProductTag', foreignKey: 'tag_id' });
 
 module.exports = {
   Product,
